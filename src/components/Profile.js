7631227482a93b1cb3b308119ecfdabb52d0adb9
Tetiana <tanichk@ukr.net>
2019-09-26 16:25:41 +0300

    Fix logic Profile page

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,29 +3,33 @@ import { firebaseApp } from './Firebase';
 import { UserProps } from "../index";
 
 export const Profile = () => {
-    const [ user, setUser ] = useState({});
+    const [ user, setUser ] = useState({UserProps});
 
     const isLogged = () => {
-        firebaseApp.database().ref('users').on('value', async snapshot => {
-            const state = await snapshot.val();
-            setUser({state});
-            console.log(state)
+        firebaseApp.database().ref('users').on('value', snapshot => {
+            snapshot.forEach(item => {
+                var childData = item.val();
+                setUser({
+                    name: childData.name,
+                    email: childData.email
+                })
+                console.log(childData)
+            })
         })
     }
 
     const signOut = () => {
         firebaseApp.auth().signOut();
+        console.log(user, 'You are logout')
     }
-
-  
-
-    console.log(user)
 
     return (
         <div>
             <h1 className="text-center">Profile Page</h1> 
             <button onClick={isLogged} className="btn btn-primary">IsLogged User</button>
             <button onClick={signOut} className="btn btn-primary">Signout</button>
+            <p>User name: {user.name}</p>
+            <p>User e-mail: {user.email}</p>
         </div>
     )
 }
